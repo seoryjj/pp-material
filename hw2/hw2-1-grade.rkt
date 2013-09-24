181c53f54c78edcda7cc8f7759f44dcb8b30a8f4
Jeehoon Kang <jhkang@ropas.snu.ac.kr>
2013-09-24 14:56:31 +0900

    Add hw2 grading examples.

--- hw2/hw2-1-grade.rkt
+++ hw2/hw2-1-grade.rkt
@@ -7,6 +7,14 @@
   (begin
     (printf "zipper\n")
 		(output (lambda () (equal? (zipper '(1) '(2)) '(1 2))))
-		(output (lambda () (equal? (zipper '(1 3 5) '(2 4)) '(1 2 3 4 5))))))
+		(output (lambda () (equal? (zipper '(1 3 5) '(2 4)) '(1 2 3 4 5))))
+		(output (lambda () (equal? (zipper '() '(1 2 3)) '(1 2 3))))
+		(output (lambda () (equal? (zipper '(1 2 3) '()) '(1 2 3))))
+		(output (lambda () (equal? (zipper '(1) '()) '(1))))
+		(output (lambda () (equal? (zipper '() '(1)) '(1))))
+		(output (lambda () (equal? (zipper '(1 2 3 4 5) '(6 7 8 9 10)) '(1 6 2 7 3 8 4 9 5 10))))
+		(output (lambda () (equal? (zipper '(10 9 8 7 6) '(5 4 3)) '(10 5 9 4 8 3 7 6))))
+		(output (lambda () (equal? (zipper '(10 9 8) '(7 6 5 4 3)) '(10 7 9 6 8 5 4 3))))
+		(output (lambda () (equal? (zipper '() '()) '())))))
 
 (zipperGrade)
