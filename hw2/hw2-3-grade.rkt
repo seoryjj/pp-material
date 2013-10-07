3dbd1d00953fe8786f4bf87bd8e8cc188adb5f10
Jeehoon Kang <jhkang@ropas.snu.ac.kr>
2013-10-07 18:06:45 +0900

    Modify hw2-3-grade; add negative input cases.

--- hw2/hw2-3-grade.rkt
+++ hw2/hw2-3-grade.rkt
@@ -13,8 +13,8 @@
     (output (lambda () (equal? ((iter 5 (lambda (x) (- 1 x))) 1) 0)))
     (output (lambda () (equal? ((iter 10 (lambda (x) (+ 2 x))) 0) 20)))
     (output (lambda () (equal? ((iter 10 (lambda (x) (+ 3 x))) 1) 31)))
-    (output (lambda () (equal? ((iter 10 (lambda (x) (* 3 x))) 1) (* 243 243))))
-    (output (lambda () (equal? ((iter 10 (lambda (x) (* 4 x))) 1) (* 1024 1024))))
-    (output (lambda () (equal? ((iter 10 (lambda (x) (- 1 x))) 1) 1)))))
+    (output (lambda () (equal? ((iter (- 0 10) (lambda (x) (* 3 x))) 1) (* 243 243))))
+    (output (lambda () (equal? ((iter (- 0 10) (lambda (x) (* 4 x))) 1) (* 1024 1024))))
+    (output (lambda () (equal? ((iter (- 0 10) (lambda (x) (- 1 x))) 1) 1)))))
 
 (iterGrade)
