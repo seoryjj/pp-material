1f3b1ea502a3deac0ac2ddcf1e3a099e899c48f7
Jeehoon Kang <jhkang@ropas.snu.ac.kr>
2013-10-07 18:01:45 +0900

    Change hw3-3-grader; it checks with the old and the new versions of graders, and mark correct if any of them says OK.

--- hw3/hw3-3-grade.rkt
+++ hw3/hw3-3-grade.rkt
@@ -35,7 +35,56 @@
     (if (equal? i 0) (cons i acc)
         (f (- i 1) (cons i acc)))))
 
-(define (check-maze maze)
+(define (check-maze-old maze)
+  (define n (car (car maze)))
+  (define m (cdr (car maze)))
+  (define (valid-location x y)
+    (and (<= 0 x) (< x n) (<= 0 y) (< y m)))
+  (define lst (cdr maze))
+  (define (lookup x y d)
+    (let* ([col (index-of lst x)]
+           [cell (index-of col y)])
+      (index-of cell d)))
+  (define tops (filter (lambda (x) (lookup x 0 0)) (range-to n)))
+  (define bots (filter (lambda (x) (lookup x (- m 1) 3)) (range-to n)))
+  (cond
+   [(not (equal? (length tops) 1)) #f]
+   [(not (equal? (length bots) 1)) #f]
+   [else
+    (begin
+      (define top (car tops))
+      (define bot (car bots))
+      (define visit
+        (let fixpt ([visit (set (cons top 0))]
+                    [worklist (set (cons top 0))])
+          (if
+           (set-empty? worklist) visit
+           (let*
+               ([xy (set-first worklist)]
+                [wl (set-rest worklist)]
+                [x (car xy)]
+                [y (cdr xy)]
+                [vw
+                 (foldl
+                  (lambda (d vw)
+                    (begin
+                      (define v (car vw))
+                      (define w (cdr vw))
+                      (define nxy (move-location x y d))
+                      (define nx (car nxy))
+                      (define ny (cdr nxy))
+                      (if (and
+                           (valid-location nx ny)
+                           (lookup x y d)
+                           (not (set-member? v nxy)))
+                          (cons (set-add v nxy) (set-add w nxy))
+                          vw)))
+                  (cons visit wl)
+                  (range-to 6))])
+             (fixpt (car vw) (cdr vw))))))
+      (set-member? visit (cons bot (- m 1))))]))
+
+(define (check-maze-new maze)
   (define n (car (car maze)))
   (define m (cdr (car maze)))
   (define (valid-location x y)
@@ -93,6 +142,9 @@
                   (range-to 6))])
              (fixpt (car vw) (cdr vw))))))
       (set-member? visit (cons bot (- m 1))))]))
+
+(define (check-maze maze)
+  (or (check-maze-old maze) (check-maze-new maze)))
 
 (define maze1 (init-maze 3 3))
 (define maze2 (open-n 1 0 maze1))
