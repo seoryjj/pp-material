dd20640710cceb766d5d7cc6ef1743cb21d6d43e
Jeehoon Kang <jhkang@ropas.snu.ac.kr>
2013-10-16 13:26:01 +0900

    Add FSM for 20131016.

---
+++ 20131016/fsm.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+
+(define init-fsm ; init-fsm: fsm
+  (raise "TODO"))
+
+(define (add-rule-fsm curstate input newstate output fsm) ; add-rule-fsm: state * input * state * output * fsm -> fsm
+  (raise "TODO"))
+
+(define (step-fsm curstate input fsm) ; step-fsm: state * input * fsm -> state X output
+  (raise "TODO"))
+
+(define (run-fsm curstate inputs fsm) ; run-stem: state * input list * fsm -> state X output list
+  (cond ([(equal? '() inputs) (cons curstate '())]
+         [else
+          (let* ([mid (step-fsm curstate (car inputs) fsm)]
+                 [midstate (car mid)]
+                 [midoutput (cdr mid)]
+                 [end (run-fsm midstate (cdr inputs) fsm)]
+                 [endstate (car end)]
+                 [endoutputs (cdr end)])
+            (cons endstate (cons midoutput endoutputs)))])))
+
+(define fsm1 init-fsm)
+(define fsm2 (add-rule-fsm "initial" "insert-coin" "coined" "" fsm1))
+(define fsm3 (add-rule-fsm "initial" "push-cola" "initial" "" fsm2))
+(define fsm4 (add-rule-fsm "initial" "push-cider" "initial" "" fsm3))
+(define fsm5 (add-rule-fsm "initial" "push-return" "initial" "" fsm4))
+(define fsm6 (add-rule-fsm "coined" "insert-coin" "coined" "give-coin" fsm5))
+(define fsm7 (add-rule-fsm "coined" "push-cola" "initial" "give-cola" fsm6))
+(define fsm8 (add-rule-fsm "coined" "push-cider" "initial" "give-cider" fsm7))
+(define fsm9 (add-rule-fsm "coined" "push-return" "initial" "give-coin" fsm8))
+
+; output: (cons "initial" (list "" "give-cola" "" "give-coin" "give-cider" ""))
+(run-fsm "initial" (list "insert-coin" "push-cola" "insert-coin" "insert-coin" "push-cider" "push-cider") fsm9)
