c411690db64a603588c069ec69080947d7e9b98c
Sungkeun Cho <skcho@ropas.snu.ac.kr>
2013-10-23 18:04:38 +0900

    fsm solution

---
+++ 20131023/fsm_imp_sol.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+; imperative programming
+
+(define fsm null)
+(define state "initial")
+
+(define (init-fsm) ; init-fsm: unit
+  (set! fsm null)
+  (set! state "initial"))
+
+(define (add-rule-fsm curstate input newstate output) ; add-rule-fsm: state * input * state * output -> unit
+  (set! fsm (cons (cons (cons curstate input) (cons newstate output)) fsm)))
+
+(define (step-fsm input) ; step-fsm: input -> output
+  (let ((input-state state)
+        (output "nothing"))
+    (for ((rule fsm))
+         (when (equal? (cons input-state input) (car rule))
+               (set! state (cadr rule))
+               (set! output (cddr rule))))
+    output))
+
+(define (run-fsm inputs) ; run-fsm: input list -> output list
+  (let ((output-list null))
+    (for ((input inputs))
+         (let ((output (step-fsm input)))
+           (set! output-list (append output-list (list output)))))
+    output-list))
+
+(init-fsm)
+(add-rule-fsm "initial" "insert-coin" "coined" "nothing")
+(add-rule-fsm "initial" "push-cola" "initial" "nothing")
+(add-rule-fsm "initial" "push-cider" "initial" "nothing")
+(add-rule-fsm "initial" "push-return" "initial" "nothing")
+(add-rule-fsm "coined" "insert-coin" "coined" "coin")
+(add-rule-fsm "coined" "push-cola" "initial" "cola")
+(add-rule-fsm "coined" "push-cider" "initial" "cider")
+(add-rule-fsm "coined" "push-return" "initial" "coin")
+
+(and
+ (equal? "initial" state)
+ (equal?
+  (list "nothing" "cola" "nothing" "coin" "cider" "nothing")
+  (run-fsm (list "insert-coin" "push-cola" "insert-coin" "insert-coin" "push-cider" "push-cider"))))
